f583e6f9f9b9da0b64475ab782a8a28c0c72f00a
mattrussell33 <matt.russell33@gmail.com>
2018-01-18 16:24:30 +0000

    Update CICD_Eval.sh

--- CICD_Eval.sh
+++ CICD_Eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REGEX="\"failed\":\[\{"
-RESPONSE=`curl --insecure -X GET http://threadfix.nttsecuritylab.co.uk:8080/rest/policy/status/application/5/evaluate?apiKey={APIKEY}&fromDate=2017-11-23&toDate=2017-11-31`
+RESPONSE=`curl --insecure -X GET http://mythreadfixurl:8080/rest/policy/status/application/5/evaluate?apiKey={APIKEY}&fromDate=2017-11-23&toDate=2017-11-31`
 #echo "${BASH_REMATCH[0]}"
 
 if [[ $RESPONSE =~ $REGEX ]]; then
